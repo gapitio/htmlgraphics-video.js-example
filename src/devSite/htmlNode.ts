f378597b5aef909fe4b206c4cfdfae78bcbcf69d
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-09-23 10:32:28 +0200

    feat: add fluid size to video

--- src/devSite/htmlNode.ts
+++ src/devSite/htmlNode.ts
@@ -7,4 +7,13 @@ if (!shadowContainer) throw new Error("Could not find shadow container.");
 window.htmlNode = shadowContainer.attachShadow({ mode: "open" }) as HTMLNode;
 
 htmlNode.onpanelupdate = () => {};
-htmlNode.innerHTML = `<style>@import "build/style.css"</style><div>${html}</div>`;
+htmlNode.innerHTML = `<style>@import "build/style.css"</style>${html}`;
+
+const htmlDocument = htmlNode.children[1] as
+  | HTMLElement
+  | (HTMLElement & SVGElement);
+
+htmlDocument.setAttribute("height", "100%");
+htmlDocument.setAttribute("width", "100%");
+htmlDocument.style.height = "100%";
+htmlDocument.style.width = "100%";
